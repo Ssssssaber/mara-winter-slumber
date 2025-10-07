f18b46a9468fecd5f21ad3895e42df72b20c7885
Ssssssaber <bladimir.lobanov@gmail.com>
2025-10-07 19:46:02 +0700

    (fix): camera movement

--- scripts/3d/camera/cameracontroller.gd
+++ scripts/3d/camera/cameracontroller.gd
@@ -6,6 +6,6 @@ var direction : Vector3 = Vector3.ZERO
 
 func _process(delta: float) -> void:
     direction.x = Input.get_axis("ui_left", "ui_right")
-    direction.z = Input.get_axis("ui_down", "ui_up")
+    direction.z = Input.get_axis("ui_up", "ui_down")
 
     position += direction.normalized() * speed * delta
