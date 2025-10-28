341f4783174239f4aafdbeed00a80ff228864d1b
Ssssssaber <bladimir.lobanov@gmail.com>
2025-10-28 22:42:40 +0700

    (udpate): speed buff implementation

--- scripts/3d/characters/mara/mara_abilities_manager.gd
+++ scripts/3d/characters/mara/mara_abilities_manager.gd
@@ -1,15 +1,22 @@
 extends Node
 
+@export var speed_buff_modifier : float = 1.5
+@export var speed_buff_duration : float = 1.5
+
+@export var ignore_ghosts_duration : float = 5.0
+
+var _mara : Node3D
 var _canvas_controller : Node
 
 func init_with_canvas_controller(canvas_controller : Node) -> void:
+	_mara = get_parent()
 	_canvas_controller = canvas_controller
 	_canvas_controller = GameManager.GetCanvasControl()
 	_canvas_controller.speed_buff_activated.connect(_on_speed_buff_activated)
 	_canvas_controller.ghost_ignore_activated.connect(_on_ingnore_ghost_debuff_activated)
 
 func _on_speed_buff_activated() -> void:
-	print("speed buff")
+	_mara.movement.apply_speed_modifier(Constants.MARA_SPEED_BUFF, 1.5, 3)
 	pass
 	
 func _on_ingnore_ghost_debuff_activated() -> void:
--- scripts/shared/constants.gd
+++ scripts/shared/constants.gd
@@ -1,4 +1,7 @@
 extends Node
 
 const GHOST_MOVEMENT_MODIFIER : String = "ghost_slow"
+const MARA_SPEED_BUFF : String = "speed_buff"
+const MARA_IGNORE_GHOSTS : String = "ignore_ghosts"
+
 const INDEFINITE_DURATION : float = -1.0
