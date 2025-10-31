770a2dcbff0470595a24e369703817d6c8a5b8bc
Ssssssaber <bladimir.lobanov@gmail.com>
2025-10-31 11:46:30 +0700

    (fix): nav agent jitter + now moving with timer

--- scripts/3d/characters/villagers/villager.gd
+++ scripts/3d/characters/villagers/villager.gd
@@ -4,17 +4,22 @@ extends CharacterBody3D
 
 @onready var navigation_agent : NavigationAgent3D = get_node("NavigationAgent3D")
 @onready var movement_system : MovementSystem = get_node("MovementSystem")
+@onready var walk_around_timer : Timer = get_node("WalkAroundTimer")
 
 var area_sphere : Node3D
 
 func _ready() -> void:
 	area_sphere = walk_around_area.get_node("CollisionShape3D")
+	walk_around_timer.timeout.connect(walk_around)
 
-func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_accept"):
-		var random_position := _random_point_on_circle(area_sphere.get_shape().radius, walk_around_area.global_transform.origin); 
-		navigation_agent.set_target_position(random_position)
+func walk_around() -> void:
+	var random_position := _random_point_on_circle(area_sphere.get_shape().radius, walk_around_area.global_transform.origin); 
+	navigation_agent.set_target_position(random_position)
 
+# func _unhandled_input(event: InputEvent) -> void:
+# 	if event.is_action_pressed("ui_accept"):
+# 		walk_around()
+	
 func _random_point_on_circle(radius : float, center_position : Vector3 = Vector3.ZERO) -> Vector3:
 	var random_andle = randf() * TAU
 
@@ -27,8 +32,9 @@ func _random_point_on_circle(radius : float, center_position : Vector3 = Vector3
 func _physics_process(_delta: float) -> void:
 	velocity = Vector3.ZERO
 	var destination = navigation_agent.get_next_path_position()
-	var local_destination = destination - global_position
-	var direction = local_destination.normalized()
-
-	velocity = direction * movement_system.base_speed
+	var distance_to_destination = global_position.distance_to(destination)
+	if distance_to_destination > 0.1:  # Only move if far enough
+		var local_destination = destination - global_position
+		var direction = local_destination.normalized()
+		velocity = direction * movement_system.base_speed
 	move_and_slide()
