57c18bf2e56606d48cfdebbdece7588a62932cce
Ssssssaber <bladimir.lobanov@gmail.com>
2025-11-01 13:46:31 +0700

    (update): ghosts vanish on after some time

--- scripts/3d/characters/ghost/ghost_behaviour.gd
+++ scripts/3d/characters/ghost/ghost_behaviour.gd
@@ -6,10 +6,12 @@ class_name GhostBehaviour
 @export var slow_duration : float = Constants.INDEFINITE_DURATION
 
 @onready var interaction_area : Area3D = get_node("InteractionArea")
+@onready var vanish_timer : Timer = get_node("VanishTimer")
 
 func _ready() -> void:
 	interaction_area.body_entered.connect(_on_interaction_area_entered)
 	interaction_area.body_exited.connect(_on_interaction_area_exited)
+	vanish_timer.timeout.connect(vanish)
 
 func _on_interaction_area_entered(body : Node3D) -> void:
 	var movement_system = body.get_node_or_null("MovementSystem")
@@ -27,3 +29,10 @@ func _on_interaction_area_exited(body : Node3D) -> void:
 		return
 	
 	movement_system.remove_modifier(Constants.GHOST_MOVEMENT_MODIFIER)
+
+func vanish() -> void:
+	var parent_node = get_parent()
+	if parent_node is PathFollower:
+		parent_node.queue_free()
+	else:
+		queue_free()
