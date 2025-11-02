f207ffd9f92b0257a047347cc6dd3c9d61300949
Ssssssaber <bladimir.lobanov@gmail.com>
2025-11-02 18:02:14 +0700

    (update): end game screen

--- scripts/3d/scenario/dialog_choices_handler.gd
+++ scripts/3d/scenario/dialog_choices_handler.gd
@@ -2,16 +2,18 @@ extends Node
 
 @export var tree_area_trigger : Node3D
 @export var evil_ghost_area_trigger : Node3D
+@export var game_ui : Control
+
+@onready var end_game_scene : PackedScene = load("res://scenes/3d/ui/end_game.tscn")
 
 signal evil_ghost_trigger_activate()
 signal special_tree_trigger_activate()
 signal start_last_home()
-signal end_game()
 
 func _ready() -> void:
 	DialogueManager.dialogue_ended.connect(_handle_dialogue_end)
 	DialogueManager.battle_ended.connect(_handle_battle_end)
-
+	
 func _handle_dialogue_end(ended_dialog_json_path : String) -> void:
 	var dialogue_id = ended_dialog_json_path
 	if (dialogue_id == Constants.TEST_DIALOG_PATH):
@@ -30,6 +32,7 @@ func _handle_battle_end(ended_dialog_json_path : String) -> void:
 		special_tree_trigger_activate.emit()
 		start_last_home.emit()
 	if (dialogue_id == Constants.LAST_BATTLE_ID):
-		print ("well played")
-		end_game.emit()
-		GameManager.call_deferred("Pause")
+		game_ui.visible = false
+		# GameManager.call_deferred("Pause")
+		var end_game_current = end_game_scene.instantiate()
+		get_tree().root.get_node("MainScene").add_child(end_game_current)
