c5ec005c15a60d9e654ffefc60728351d4269c1b
Ssssssaber <bladimir.lobanov@gmail.com>
2025-11-04 17:30:27 +0700

    (fix): tree applying buff continiously

--- scripts/3d/environment/quests/special_tree.gd
+++ scripts/3d/environment/quests/special_tree.gd
@@ -3,13 +3,32 @@ extends Node
 class_name SpecialTree
 
 @onready var interaction_area : Area3D = get_node("InteractionArea")
-@export var speed_modifier : float = 3
+@export var speed_modifier : float = 1.5
 @export var modifier_duration : float = 4.0
-@export var activated = false
+@export var apply_buff_timer : Timer
+
+@export var activated : bool = false
 
 func _ready() -> void:
-	interaction_area.body_entered.connect(_apply_buff)
-	interaction_area.body_exited.connect(_apply_buff)
+	if activated:
+		apply_buff_timer.start()
+	else:
+		apply_buff_timer.stop()
+	#interaction_area.body_entered.connect(_apply_buff)
+	#interaction_area.body_exited.connect(_apply_buff)
+	apply_buff_timer.timeout.connect(_buff_entities_in_area)
+
+func set_activate(value : bool) -> void:
+	activated = value
+	if activated:
+		apply_buff_timer.start()
+	else:
+		apply_buff_timer.stop()
+
+func _buff_entities_in_area() -> void:
+	var bodies = interaction_area.get_overlapping_bodies()
+	for body in bodies:
+		_apply_buff(body)
 
 func _apply_buff(body : Node3D):
 	if not activated:
@@ -18,7 +37,4 @@ func _apply_buff(body : Node3D):
 	var movement_system = body.get_node_or_null("MovementSystem")
 	if not movement_system:
 		return
-	print(movement_system._calculate_effective_speed())
 	movement_system.apply_speed_modifier(Constants.SPECIAL_TREE_MOVEMENT_MODIFIER, speed_modifier, modifier_duration)
-	print(movement_system._calculate_effective_speed())
-	
