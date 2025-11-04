32cc4d08246bf979d0f91414c0b80952be62f8eb
Ssssssaber <bladimir.lobanov@gmail.com>
2025-11-04 20:46:28 +0700

    (udpate): ghsot disappear animation

--- scripts/3d/characters/ghost/ghost_behaviour.gd
+++ scripts/3d/characters/ghost/ghost_behaviour.gd
@@ -4,6 +4,7 @@ class_name GhostBehaviour
 
 @export var slow_modifier : float = 0.5
 @export var slow_duration : float = Constants.INDEFINITE_DURATION
+@export var animation_manager : AnimatedSprite3D 
 
 @onready var interaction_area : Area3D = get_node("InteractionArea")
 @onready var vanish_timer : Timer = get_node("VanishTimer")
@@ -38,6 +39,9 @@ func _on_interaction_area_exited(body : Node3D) -> void:
 
 
 func vanish() -> void:
+	animation_manager.play("vanish")
+	await get_tree().create_timer(2).timeout
+
 	var parent_node = get_parent()
 	if parent_node is PathFollower:
 		parent_node.queue_free()
